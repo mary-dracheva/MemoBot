78b8ef7c28b75f5893e3e691936eb4b0e944b08a
mary-dracheva <mary.dracheva@gmail.com>
2024-06-30 19:57:10 +0300

    Initial commit

---
+++ src/main/java/com/example/bot/memo/MemoBotApplication.java
@@ -0,0 +1,14 @@
+package com.example.bot.memo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MemoBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MemoBotApplication.class, args);
+		System.out.println("Hello, Mary!!");
+	}
+
+}
---
+++ src/test/java/com/example/bot/memo/MemoBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bot.memo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MemoBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
